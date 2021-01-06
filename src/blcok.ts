d154a786049af11c8fe496bac1162a45711dde03
kimhyunjin <khjzzm@gmail.com>
2021-01-07 00:37:22 +0900

    ts block

---
+++ src/blcok.ts
@@ -0,0 +1,43 @@
+import * as CryptoJs from "crypto-js";
+
+class Block {
+    public index: number;
+    public hash: string;
+    public previousHash: string;
+    public data: string;
+    public timestamp: number;
+
+    static calculateBlockHash =
+        (index: number, previousHash: string, timestamp: number, data: string): string => CryptoJs.SHA256(index + previousHash + timestamp + data).toSting;
+
+    constructor(
+        index: number,
+        hash: string,
+        previousHash: string,
+        data: string,
+        timestamp: number
+    ) {
+        this.index = index;
+        this.hash = hash;
+        this.previousHash = previousHash;
+        this.data = data;
+        this.timestamp = timestamp;
+    }
+}
+
+
+
+
+const genessisBlock: Block = new Block(0, "20202020", "", "Hello", 123456);
+
+let blockchain: Block[] = [genessisBlock];
+
+const getBlockchain = () : Block[] => blockchain;
+
+const getLatestBlock = () : Block => blockchain[blockchain.length - 1];
+
+const getNewTimeStamp = () : number => Math.round(new Date().getTime() / 1000)
+
+//console.log(Block.calculateBlockHash(1, "2323`2323", 123, "hello genie"));
+
+export { };
